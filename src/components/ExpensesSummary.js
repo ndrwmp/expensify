64a10033d1917ef903392183e92a91d2a68c22d7
Andrew Playsted <andrewmpb@gmail.com>
2018-12-12 17:30:09 -0800

    Stylized every page

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
@@ -9,8 +10,13 @@ export const ExpensesSummary = (props) => {
     const formattedExpensesTotal = numeral(props.expensesTotal / 100).format('$0,0.00');
 
     return (
-        <div>
-            <p>Viewing {props.expensesCount} {expenseWord} totalling { formattedExpensesTotal }</p>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.expensesCount}</span> {expenseWord} totalling <span>{ formattedExpensesTotal }</span>.</h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
