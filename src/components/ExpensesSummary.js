c34b8c5dda872a4641fe093fc4d4b7b95d8efdc9
Andrew Playsted <andrewmpb@gmail.com>
2018-12-17 02:21:32 -0800

    Added message to show number of filtered expenses to Dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,13 +6,16 @@ import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
 export const ExpensesSummary = (props) => {
-    const expenseWord = (props.expensesCount === 1 ) ? 'expense' : 'expenses';
-    const formattedExpensesTotal = numeral(props.expensesTotal / 100).format('$0,0.00');
+    const filteredExpensesCount = props.unfilteredExpensesCount-props.filteredExpensesCount;
+    const filteredExpenseWord = (props.filteredExpensesCount === 1 ) ? 'expense' : 'expenses';
+    const unfilteredExpenseWord = (filteredExpensesCount === 1 ) ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(props.filteredExpensesTotal / 100).format('$0,0.00');
 
     return (
         <div className="page-header">
             <div className="content-container">
-                <h1 className="page-header__title">Viewing <span>{props.expensesCount}</span> {expenseWord} totalling <span>{ formattedExpensesTotal }</span>.</h1>
+                <h1 className="page-header__title">Viewing <span>{props.filteredExpensesCount}</span> {filteredExpenseWord} totalling <span>{ formattedExpensesTotal }</span>.</h1>
+                <h3 className="page-header__title">Filtering <span>{filteredExpensesCount}</span> {unfilteredExpenseWord} from view.</h3>
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -24,8 +27,9 @@ export const ExpensesSummary = (props) => {
 const mapStateToProps = (state) => {
     const filteredExpenses = selectExpenses(state.expenses, state.filters);
     return {
-        expensesCount: filteredExpenses.length,
-        expensesTotal: selectExpensesTotal(filteredExpenses)
+        unfilteredExpensesCount: state.expenses.length,
+        filteredExpensesCount: filteredExpenses.length,
+        filteredExpensesTotal: selectExpensesTotal(filteredExpenses)
     };
 };
 
